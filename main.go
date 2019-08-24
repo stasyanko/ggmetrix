d29ed5b2319b95588fb7e3cac63e7dc92ea54c5f
Stas <kapilka06@gmail.com>
2019-08-24 20:10:40 +0300

    static assets

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import (
 	"github.com/gobuffalo/packr"
 )
 
+// building GOPATH/bin/packr main.go
 func main() {
-	box := packr.NewBox("./templates")
+	box := packr.NewBox("./static")
 
 	http.Handle("/", http.FileServer(box))
 	http.ListenAndServe(":3000", nil)
