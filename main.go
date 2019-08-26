93c545c57cea58b23a0ea5d4d5862a0d193db799
Stas <kapilka06@gmail.com>
2019-08-26 18:04:06 +0300

    embed statis assets to gin build

--- main.go
+++ main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"net/http"
 
-	"github.com/gobuffalo/packr"
+	rice "github.com/GeertJohan/go.rice"
+	//TODO: fork this repo and replace its link to fork
+	"github.com/foolin/gin-template/supports/gorice"
+	"github.com/gin-gonic/gin"
 )
 
-// building GOPATH/bin/packr main.go
 func main() {
-	box := packr.NewBox("./static")
 
-	http.Handle("/", http.FileServer(box))
-	http.ListenAndServe(":3000", nil)
+	router := gin.Default()
+
+	// servers other static files
+	staticBox := rice.MustFindBox("static")
+	router.StaticFS("/static", staticBox.HTTPBox())
+
+	//new template engine
+	router.HTMLRender = gorice.New(rice.MustFindBox("views"))
+
+	// Routes
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{"title": "ggmetrix"})
+	})
+
+	router.GET("/chart", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "chart.html", gin.H{"title": "ggmetrix"})
+	})
+
+	// Start server
+	router.Run(":8000")
 }
